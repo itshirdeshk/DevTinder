29f649e37b234770171f1cfb3b056afc1dc3b77b
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2025-02-17 16:45:12 +0530

    feat: exploring HTTP mehods

--- src/app.js
+++ src/app.js
@@ -2,14 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use("/", (req, res) => {
-    res.send('Hello World');
+// This will only handle GET requests to the /user route.
+app.get('/user', (req, res) => {
+    res.send({
+        firstName: "Hirdesh",
+        lastName: "Khandelwal"
+    });
 });
 
-app.use("/home", (req, res) => {
-    res.send('Hello World from Home');
-})
+app.post('/user', (req, res) => {
+    res.send("Data successfully saved to the DB");
+});
 
-app.listen(3000, () => {  
+// This will handle all requests to the /user route.
+app.use('/test', (req, res) => {
+    res.send("Hello from Test");
+});
+
+app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
