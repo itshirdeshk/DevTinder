23ffe9e0f7cee14e6ff5696fc2aaa0516b4c6682
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2025-02-20 20:47:24 +0530

    feat: exploring validator library

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = mongoose.Schema({
     firstName: {
@@ -13,7 +14,12 @@ const userSchema = mongoose.Schema({
         required: true,
         unique: true,
         trim: true,
-        lowercase: true
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                res.status(400).send("Provided email is not a valid email.")
+            }
+        }
     },
     password: {
         type: String,
@@ -34,7 +40,12 @@ const userSchema = mongoose.Schema({
         }
     },
     profilePhoto: {
-        type: String
+        type: String,
+        validate(value) {
+            if (!validator.isURL(value)) {
+                res.status(400).send("Provided profilePhoto URL is not a valid URL.")
+            }
+        }
     },
     skills: {
         type: [String]
